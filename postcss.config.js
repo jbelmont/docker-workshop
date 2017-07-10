1f0d5d55b853bc26bb6b87173853f7ea9e6c8343
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-07-09 20:05:48 -0400

    Remove content for modern web and have pure workshop content.

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = (ctx) => ({
-  map: ctx.options.map,
-  'plugins': {
-    // to edit target browsers: use "browserlist" field in package.json
-    'autoprefixer': {}
-  }
-})
